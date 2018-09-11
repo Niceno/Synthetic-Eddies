bfb3f604172204e74698b83860b807609ec9d45f
Niceno <bojan@niceno.ch>
2018-09-11 22:31:40 +0200

    Made it independent on the type of grid.

--- Generate_Fluctuations.f90
+++ Generate_Fluctuations.f90
@@ -6,7 +6,7 @@
 !----------------------------------[Modules]-----------------------------------!
   use Eddy_Mod, only: Eddy_Type
   use Flow_Mod, only: Flow_Type
-  use Mesh_Mod, only: Mesh_Type, STRUCTURED, UNSTRUCTURED
+  use Mesh_Mod, only: Mesh_Type
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
@@ -15,7 +15,7 @@
 !-----------------------------------[Locals]-----------------------------------!
   integer                 :: e, c
   real                    :: x0, y0, z0, f, v_b
-  real                    :: y_s, y_e, z_s, z_e, s
+  real                    :: srf
   type(Mesh_Type), target :: msh
 !==============================================================================!
 
@@ -27,52 +27,44 @@
   flw % w % raw(:) = 0.0
   flw % t % raw(:) = 0.0
 
-  ! Surface area ... MAKE THIS MORE GENERAL
-  if(msh % mode .eq. STRUCTURED) then
-    y_s = minval(msh % yn(:))
-    y_e = maxval(msh % yn(:))
-    z_s = minval(msh % zn(:))
-    z_e = maxval(msh % zn(:))
-    s = (y_e - y_s) * (z_e - z_s)
-  end if
+  ! Inlet surface area ...
+  srf = sum(msh % area(:))
 
   ! ... and some kind of volume ... boundary volume?
-  v_b = s * 2 * ed % sigma
+  v_b = srf * 2 * ed % sigma
 
-  if(msh % mode == STRUCTURED) then
-    do c = 1, msh % n_cells                             ! through cells
-      do e = 1, ed % n_eddies
-        x0 = (0           - ed % x(e)) / ed % len(e)
-        y0 = (msh % yc(c) - ed % y(e)) / ed % len(e)  ! cell center
-        z0 = (msh % zc(c) - ed % z(e)) / ed % len(e)  ! cell center
+  do c = 1, msh % n_cells                           ! through cells
+    do e = 1, ed % n_eddies
+      x0 = (0           - ed % x(e)) / ed % len(e)
+      y0 = (msh % yc(c) - ed % y(e)) / ed % len(e)
+      z0 = (msh % zc(c) - ed % z(e)) / ed % len(e)
 
-        !--------------------!
-        !   Shape function   !
-        !--------------------!
-        if (abs(x0)<=1.0 .and. abs(y0)<=1.0 .and. abs(z0)<=1.0) then
-          f = sqrt(1.5) * (1.0 - abs(x0)) *                     &
-              sqrt(1.5) * (1.0 - abs(y0)) *                     &
-              sqrt(1.5) * (1.0 - abs(z0))
+      !--------------------!
+      !   Shape function   !
+      !--------------------!
+      if (abs(x0)<=1.0 .and. abs(y0)<=1.0 .and. abs(z0)<=1.0) then
+        f = sqrt(1.5) * (1.0 - abs(x0)) *                     &
+            sqrt(1.5) * (1.0 - abs(y0)) *                     &
+            sqrt(1.5) * (1.0 - abs(z0))
 
-          flw % u % raw(c) = flw % u % raw(c) +                 &
-                             sqrt(v_b/ed % len(e)**3) *         &
-                             ed % x_int(e)*f
+        flw % u % raw(c) = flw % u % raw(c) +                 &
+                           sqrt(v_b/ed % len(e)**3) *         &
+                           ed % x_int(e)*f
 
-          flw % v % raw(c) = flw % v % raw(c) +                 &
-                             sqrt(v_b/ed % len(e)**3) *         &
-                             ed % y_int(e)*f
+        flw % v % raw(c) = flw % v % raw(c) +                 &
+                           sqrt(v_b/ed % len(e)**3) *         &
+                           ed % y_int(e)*f
 
-          flw % w % raw(c) = flw % w % raw(c) +                 &
-                             sqrt(v_b/ed % len(e)**3) *         &
-                             ed % z_int(e)*f
+        flw % w % raw(c) = flw % w % raw(c) +                 &
+                           sqrt(v_b/ed % len(e)**3) *         &
+                           ed % z_int(e)*f
 
-          flw % t % raw(c) = flw % t % raw(c) +                 &
-                             sqrt(v_b/ed % len(e)**3) *         &
-                             ed % t_int(e)*f
-        end if
-      end do
+        flw % t % raw(c) = flw % t % raw(c) +                 &
+                           sqrt(v_b/ed % len(e)**3) *         &
+                           ed % t_int(e)*f
+      end if
     end do
-  end if
+  end do
 
   flw % u % raw(:) = flw % u % raw(:) / sqrt(real(ed % n_eddies, 8))
   flw % v % raw(:) = flw % v % raw(:) / sqrt(real(ed % n_eddies, 8))
