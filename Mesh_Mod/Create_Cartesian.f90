ab090a3253a8358ed66522f3cd069abca825af08
Niceno <bojan@niceno.ch>
2018-09-11 20:36:51 +0200

    Stores coordinates as one-dimensional arrays for all nodes and cells.

--- Mesh_Mod/Create_Cartesian.f90
+++ Mesh_Mod/Create_Cartesian.f90
@@ -6,10 +6,19 @@
   type(Mesh_Type) :: msh
   integer         :: ny, nz
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: j, k
+  integer :: j, k, n, c
+  real, allocatable :: yn(:), zn(:), yc(:), zc(:), dy(:), dz(:)
 !==============================================================================!
 
   msh % mode     =  STRUCTURED
+
+  ! Allocate local memory
+  allocate(yn(ny  ));  yn(:) = 0.0
+  allocate(zn(nz  ));  zn(:) = 0.0
+  allocate(yc(ny-1));  yc(:) = 0.0
+  allocate(zc(nz-1));  zc(:) = 0.0
+  allocate(dy(ny-1));  dy(:) = 0.0
+  allocate(dz(nz-1));  dz(:) = 0.0
 
   ! Store number of nodes and cells
   msh % ny       =  ny
@@ -19,28 +28,50 @@
                  * (msh % nz - 1)
 
   ! Allocate memory
-  allocate(msh % yn(msh % ny))
-  allocate(msh % zn(msh % nz))
-  allocate(msh % yc(msh % ny - 1))
-  allocate(msh % zc(msh % nz - 1))
+  allocate(msh % yn  (msh % n_nodes))
+  allocate(msh % zn  (msh % n_nodes))
+  allocate(msh % yc  (msh % n_cells))
+  allocate(msh % zc  (msh % n_cells))
+  allocate(msh % area(msh % n_cells))
 
   ! Initialize "y" coordinates, they range from zero to two
-  do j = 1, msh % ny
-    msh % yn(j) = (j-1) * 2.00 / (msh % ny-1)
+  do j = 1, ny
+    yn(j) = (j-1) * 2.00 / real(ny-1)
   end do
 
-  do j = 1, msh % ny - 1
-    msh % yc(j) = ( msh % yn(j) + msh % yn(j+1) ) * 0.5
+  do j = 1, ny - 1
+    yc(j) = ( yn(j) + yn(j+1) ) * 0.5
+    dy(j) = ( yn(j+1) - yn(j) )
   end do
 
   ! Compute "z" cooridnates for nodes and ...
   ! ... cells, assuming domain width is 2 PI
-  do k = 1, msh % nz
-    msh % zn(k) = (k-1) * 6.28 / (msh % nz-1)
+  do k = 1, nz
+    zn(k) = (k-1) * 6.28 / real(nz-1)
   end do
 
+  do k = 1, nz - 1
+    zc(k) = ( zn(k) + zn(k+1) ) * 0.5
+    dz(k) = ( zn(k+1) - zn(j) )
+  end do
+
+  ! Copy linear node coordinates to full mesh coordinates
+  do k = 1, msh % nz
+    do j = 1, msh % ny
+      n = (k - 1) * msh % ny + j        ! find linear address for the node
+      msh % yn(n) = yn(j)
+      msh % zn(n) = zn(k)
+    end do
+  end do
+
+  ! Copy linear cell coordinates to full mesh coordinates
   do k = 1, msh % nz - 1
-    msh % zc(k) = ( msh % zn(k) + msh % zn(k+1) ) * 0.5
+    do j = 1, msh % ny - 1
+      c = (k - 1) * (msh % ny - 1) + j  ! find linear address for the cell
+      msh % yc(c) = yc(j)
+      msh % zc(c) = zc(k)
+      msh % area(c) = dy(j) * dz(k)
+    end do
   end do
 
   end subroutine
